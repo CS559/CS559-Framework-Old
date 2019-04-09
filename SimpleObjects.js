8d1e2f069834d42bcbf3421244aac947a0b9307a
gleicher (home) <gleicher@cs.wisc.edu>
2019-04-09 08:12:10 -0500

    Add SquareSign simple object

--- SimpleObjects.js
+++ SimpleObjects.js
@@ -93,6 +93,50 @@ export class GrSphere extends GrObject {
     }
 }
 
+export class GrSquareSign extends GrObject {
+    /**
+     * 
+     * @param {Object} [params] 
+     * @param {string | number} [params.color]
+     * @param {THREE.Texture} [params.map]
+     * @param {THREE.Material} [params.material]
+     * @param {number} [params.x]
+     * @param {number} [params.y]
+     * @param {number} [params.z]
+     * @param {number} [params.size]
+     * @param {Array<string|Array>} [paramInfo ]
+     */
+    constructor(params={},paramInfo=[]) {
+        // make a square out of triangles
+        let geom=new T.Geometry();
+        let size = params.size || 0.5;
+        geom.vertices.push(new T.Vector3(-size,-size,0)); let uv0=new T.Vector2(0,0);
+        geom.vertices.push(new T.Vector3( size,-size,0)); let uv1=new T.Vector2(1,0);
+        geom.vertices.push(new T.Vector3(-size, size,0)); let uv2=new T.Vector2(0,1);
+        geom.vertices.push(new T.Vector3( size, size,0)); let uv3=new T.Vector2(1,1);
+        geom.faces.push(new T.Face3(0,1,2));
+        geom.faces.push(new T.Face3(1,3,2));
+        geom.computeFaceNormals();
+        geom.faceVertexUvs = [ [[uv0, uv1, uv2], [uv1,uv3,uv2]] ];
+        let material;
+        if (params.material) {
+            material = params.material;
+        } else {
+            let matprops = { side: T.DoubleSide };
+            matprops.color = params.color ? params.color : 0xFFFFFF;
+            if (params.map) matprops.map = params.map;
+            material = new T.MeshStandardMaterial(matprops);
+            console.log(matprops);
+        }
+        let mesh = new T.Mesh(geom,material);
+        super(`SquareSign-${simpleObjectCounter++}`,mesh,paramInfo);
+        // put the object in its place
+        mesh.position.x = params.x ? Number(params.x) : 0;
+        mesh.position.y = params.y ? Number(params.y) : 0;
+        mesh.position.z = params.z ? Number(params.z) : 0;
+    }
+}
+
 export class GrTorusKnot extends GrObject {
     /**
      * @param {Object} [params]
