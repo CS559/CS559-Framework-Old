a9ab443ff1d7ffa8b5179150e01139165ebbc9d3
LswaN58 <lwswanson2@wisc.edu>
2019-04-22 13:17:59 -0500

    Forgot to flip view direction on main camera

--- GrWorld.js
+++ GrWorld.js
@@ -531,7 +531,7 @@ export class GrWorld {
         this.active_object.objects[0].add(this.camera);
         let bbox = new T.Box3();
         bbox.setFromObject(this.active_object.objects[0]);
-        this.camera.position.set(0, bbox.max.y-bbox.min.y, 1.5*(bbox.max.z-bbox.min.z));
+        this.camera.position.set(0, bbox.max.y-bbox.min.y, -1.5*(bbox.max.z-bbox.min.z));
         this.solo_camera.position.set(0, bbox.max.y-bbox.min.y, -1.5*(bbox.max.z-bbox.min.z));
         // Set look direction
         let target = this.active_object.objects[0].position;
